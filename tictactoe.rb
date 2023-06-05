88a5063003c33b192e528f1119adaf1b8a181f0b
pacianoseth <feintimg@gmail.com>
2023-06-05 23:03:27 +0800

    learned how to create a board with indexed array inside of it

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,12 +1,13 @@
 puts "Welcome to TIC-TAC-TOE!"
 
+board = ["2","z","2","3","s","g","A","h","V"]
 
-def print_board
-    puts row = ["   " "|" "   " "|" "   "]
+def print_board(board)
+    puts row = ["#{board[0]} " "|" " #{board[1]} " "|" " #{board[2]}  "]
     puts separator = "-----------"
-    puts row
+    puts row =  ["#{board[3]} " "|" " #{board[4]} " "|" " #{board[5]}  "]
     puts separator
-    puts row
+    puts row = ["#{board[6]} " "|" " #{board[7]} " "|" " #{board[6]}  "]
 end
 
-print_board
+print_board(board)
