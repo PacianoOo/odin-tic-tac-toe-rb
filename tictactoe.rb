7f92294f5ca599d81b32c9cb67bfec351713dbb0
Seth <129527357+PacianoOo@users.noreply.github.com>
2023-06-05 22:53:04 +0800

    Add files via upload
    first commit

---
+++ tictactoe.rb
@@ -0,0 +1,12 @@
+puts "Welcome to TIC-TAC-TOE!"
+
+
+def print_board
+    puts row = ["   " "|" "   " "|" "   "]
+    puts separator = "-----------"
+    puts row
+    puts separator
+    puts row
+end
+
+print_board
